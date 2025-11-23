35d9b48edd01698605db5176bf541c5ddb1c79b7
Abhishek <abhishekpaul3298@gmail.com>
2025-11-23 14:09:40 +0000

    hhhhajjj

--- resources/views/pages/appearance-settings.blade.php
+++ resources/views/pages/appearance-settings.blade.php
@@ -1,7 +1,7 @@
 @php /** @var \App\Filament\Pages\AppearanceSettings $this */ @endphp
 <x-filament-panels::page>
     {{ $this->form }}
-    <x-filament-actions::actions :actions="[
+    <x-filament-actions::group :actions="[
         \Filament\Actions\Action::make('save')->label('Save')->submit('save')->color('primary'),
     ]" alignment="left" />
 </x-filament-panels::page>
